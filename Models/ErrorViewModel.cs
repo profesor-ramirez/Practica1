e1e53f6099f853406c599d49d027561de098d35a
Santiago Ramirez <sr10973@unphu.edu.do>
2024-05-16 10:06:02 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
